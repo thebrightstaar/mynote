150f7b1b876ecd2155c3b80e95081d70e4aa44ef
thebrightstaar <thebrightstaar@gmail.com>
2021-02-16 13:29:24 +0200

    modified

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,29 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    mynote
+                </div>
+
+                <div class="links">
+                    <a href="{{ route('notes') }}">note</a>
+        
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
@@ -16,6 +39,8 @@
 
                     {{ __('You are logged in!') }}
                 </div>
+                <div>
+
             </div>
         </div>
     </div>
